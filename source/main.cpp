9ac14771b3fcaad18676d9c50d46f0a5a5e8a4ca
ibis-hdl <83083329+ibis-hdl@users.noreply.github.com>
2025-02-16 14:26:26 +0000

    Enforce Sanitizer Failures

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,6 @@ auto main() -> int
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
-  void* x = new int;  // check sanitize
+
   return 0;
 }
--- test/source/app_test.cpp
+++ test/source/app_test.cpp
@@ -7,3 +7,16 @@ TEST_CASE("Name is app", "[library]")
   auto const lib = library {};
   REQUIRE(lib.name == "app");
 }
+
+TEST_CASE("Sanitize Leak Test", "[detected memory leaks]")
+{
+  int* x = new int;
+  REQUIRE(x != nullptr);
+}
+
+TEST_CASE("Sanitize Test", "[heap-use-after-free]")
+{
+  int* x = new int;
+  delete x;
+  REQUIRE(*x == 42);
+}
